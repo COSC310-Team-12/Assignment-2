4baa3cd63cea85f263a8e90a051c5b4c65cd663d
Brian Z <brianzhouzc@gmail.com>
2021-03-03 23:59:06 -0800

    Move bootstrap javascript to local

--- src/renderer.js
+++ src/renderer.js
@@ -2,7 +2,7 @@
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
 let $ = jQuery = require('jquery');
-const {ipcRenderer} = require('electron')
+const { ipcRenderer } = require('electron')
 const connection = new WebSocket('ws://51.141.164.131:10001/websocket');
 
 connection.onopen = () => {
@@ -22,23 +22,23 @@ connection.onmessage = event => {
     addToChat(JSON.parse(event.data)["text"], 'left');
 };
 
-$("#close").click(function(e) {
+$("#close").click(function (e) {
     ipcRenderer.sendSync('close_app');
 });
 
-$("#minimize").click(function(e) {
+$("#minimize").click(function (e) {
     ipcRenderer.sendSync('minimize_app');
 });
 
-var Message = function(arg) {
+var Message = function (arg) {
     this.text = arg.text, this.message_side = arg.message_side;
-    this.draw = function(_this) {
-        return function() {
+    this.draw = function (_this) {
+        return function () {
             var $message;
             $message = $($('.message_template').clone().html());
             $message.addClass(_this.message_side).find('.text').html(_this.text);
             $('.messages').append($message);
-            return setTimeout(function() {
+            return setTimeout(function () {
                 return $message.addClass('appeared');
             }, 0);
         };
@@ -46,14 +46,14 @@ var Message = function(arg) {
     return this;
 };
 
-var sendMessage = function(text) {
-    if (text === "") {return;}
+var sendMessage = function (text) {
+    if (text === "") { return; }
     let message = {};
     message["text"] = text;
     connection.send(JSON.stringify(message));
 };
 
-var addToChat = function(text, message_side) {
+var addToChat = function (text, message_side) {
     var $messages, message;
     if (text.trim() === '') {
         return;
@@ -73,17 +73,17 @@ var addToChat = function(text, message_side) {
 
 var getMessageText, message_side;
 message_side = 'right';
-getMessageText = function() {
+getMessageText = function () {
     var $message_input;
     $message_input = $('.message_input');
     return $message_input.val();
 };
 
-$('.send_message').click(function(e) {
+$('.send_message').click(function (e) {
     sendMessage(getMessageText())
     return addToChat(getMessageText(), 'right');
 });
-$('.message_input').keyup(function(e) {
+$('.message_input').keyup(function (e) {
     if (e.which === 13) {
         sendMessage(getMessageText())
         return addToChat(getMessageText(), 'right');
