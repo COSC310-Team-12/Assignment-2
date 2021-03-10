f7adc9c0a0c11948b562417d05288e2d506e9597
Brian Z <brianzhouzc@gmail.com>
2021-03-10 13:59:10 -0800

    Added bot response logic

--- src/renderer.js
+++ src/renderer.js
@@ -6,11 +6,16 @@ const { ipcRenderer } = require('electron');
 const { createPopper } = require('@popperjs/core');
 const bootstrap = require('bootstrap');
 
-const connection = new WebSocket('ws://51.141.164.131:10001/websocket');
+const connection = new WebSocket('ws://127.0.0.1:10001/websocket');
+let connected = false;
 
 connection.onopen = () => {
-    showNotification('Connected to server');
-    console.log('connected');
+    sendMessage("[START]");
+    setTimeout(() => { 
+        showNotification('Connected to server');
+        connected = true;
+        console.log('connected');
+     }, 1000);
 };
 
 connection.onclose = () => {
@@ -23,13 +28,13 @@ connection.onerror = error => {
 
 connection.onmessage = event => {
     console.log('received', event.data);
-    addToChat(JSON.parse(event.data)["text"], 'left');
+    var response = processResponse(event.data);
+    addToChat(response.message.phrase, 'left');
 };
 
 $("#close").click(function (e) {
     addToChat = function () { };
     showNotification = function () { };
-    sendMessage("[DONE]");
     setTimeout(() => { connection.close(); }, 1000);
     setTimeout(() => { ipcRenderer.sendSync('close_app'); }, 1000);
 });
@@ -55,11 +60,82 @@ var Message = function (arg) {
 };
 
 var sendMessage = function (text) {
-    if (text === "") { return; }
+    if (!connected && text === "") { return; }
     let message = {};
     message["text"] = text;
     connection.send(JSON.stringify(message));
 };
+
+function processResponse(rawdata) {
+    rawdata = JSON.parse(rawdata);
+
+    var msg = {
+        phrase: rawdata["text"],
+        probability: 1
+    };
+
+    var response = {
+        message: msg,
+        beam_texts: transformProbability(rawdata["beam_texts"]),
+        quick_reply: rawdata["quick_replies"]
+    };
+
+    if (response.beam_texts)
+        response.message = weightedRandom(response.beam_texts);
+
+    console.log('message:', response.message);
+    console.log("beam_texts:", response.beam_texts);
+    console.log("quick_reply:", response.quick_reply);
+
+    return response;
+}
+
+function transformProbability(array) {
+    if (!array) return array;
+    var dict = [];
+    var sum = 0;
+    array.forEach(function (item) {
+        //Create beam_text object
+        var beam_text = {
+            phrase: item[0],
+            probability: item[1]
+        }
+        dict.push(beam_text);
+        sum += beam_text.probability;
+    });
+
+    var sum2 = 0;
+    for (var i = 0; i < dict.length; i++) {
+        var val = dict[i].probability;
+        //Map probability to 0-1 scale.
+        var prob = 0.01 / (val / sum);
+        //Map linear scale to logrithmic scale
+        var prob = prob * (1 / (i + 1));
+
+        sum2 += prob;
+        dict[i].probability = prob;
+    }
+
+    dict.forEach(function (item) {
+        //Map probability to 0-1 scale again.
+        item.probability = item.probability / sum2;
+    });
+
+    return dict;
+}
+
+// Select an item out of an array using its probability
+function weightedRandom(array) {
+    if (!array) return array;
+
+    var weighted = weightArray(array);
+    return weighted[Math.floor(Math.random() * weighted.length)];
+}
+
+// Filles an array with the correct wieght of each object
+function weightArray(array) {
+    return [].concat(...array.map((obj) => Array(Math.ceil(obj.probability * 100)).fill(obj)));
+}
 
 var addToChat = function (text, message_side) {
     var $messages, message;
